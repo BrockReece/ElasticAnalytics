0430a3142c7553b76b28edeb9f78a4c19ab6f08b
BrockReece <brockreece@gmail.com>
2016-06-06 13:59:03 +0100

    Add Library

---
+++ src/ElasticAnalytics.php
@@ -0,0 +1,21 @@
+<?php
+namespace ElasticAnalytics;
+
+class ElasticAnalytics {
+    protected $client;
+
+    public function __construct() {
+        $this->client = $this->buildClient();
+    }
+
+    public static function buildClient() {
+        return \Elasticsearch\ClientBuilder::create()
+            ->setHosts(config('elasticquent.config.hosts'))
+            ->setRetries(config('elasticquent.config.retries'))
+            ->build();
+    }
+
+    public static function fireEvent(array $params) {
+        self::buildClient()->index($params);
+    }
+}
