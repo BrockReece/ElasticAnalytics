1727222325df43d0443cf0f378881a5490c8e924
BrockReece <brockreece@gmail.com>
2016-06-09 12:23:08 +0100

    Magic replicator method

--- src/ElasticAnalytics.php
+++ src/ElasticAnalytics.php
@@ -15,7 +15,21 @@ class ElasticAnalytics {
             ->build();
     }
 
+    public static function buildClientByIP($ip) {
+        return \Elasticsearch\ClientBuilder::create()
+            ->setHosts([$ip])
+            ->setRetries(config('elasticquent.config.retries'))
+            ->build();
+    }
+
     public static function log(array $params) {
         self::buildClient()->index($params);
+    }
+
+    public static function magicReplicator($function) {
+        foreach (config('elasticquent.config.hosts') as $ip) {
+            $client = self::buildClientByIP($ip);
+            $function($client);
+        }
     }
 }
